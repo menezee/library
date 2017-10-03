e3ab1cd74befb3110c928c10070ae861029e9de8
Erich Menezes <emenezes@thoughtworks.com>
2017-10-03 18:05:56 -0300

    add support for json response

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const http = require('http');
 const port = 3000;
 
-const httpServer = http.createServer((req, res) => res.end('hello'));
+const httpServer = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: `${req.method} received on ${req.url}` }));
+});
+
 httpServer.listen(port, () => {
     console.log(`listening on ${port}`)
 });
+

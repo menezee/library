23121b46dcc42f28d19316f1b8795c27e359af43
Erich Menezes <emenezes@thoughtworks.com>
2017-10-03 17:43:35 -0300

    refactoring arrow functions and warning server listening on x port

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const http = require('http');
 const port = 3000;
 
-function handleRequest(req, res) {
-    console.log(`req: method ${req.method}, resource: ${req.url}`);
-    res.end('hello');
-}
-
-const httpServer = http.createServer(handleRequest);
-
-httpServer.listen(port);
+const httpServer = http.createServer((req, res) => res.end('hello'));
+httpServer.listen(port, () => {
+    console.log(`listening on ${port}`)
+});

0f1084c7c6117501aee02a76c19ed840510b7aa0
Erich Menezes <emenezes@thoughtworks.com>
2017-10-05 18:24:00 -0300

    add express dependency and refactoring in index to use express

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
-import http from 'http'
+const express = require('express'); // const app = const express = require('express')();
+const app = express();
 const port = 3000;
 
-const httpServer = http.createServer((req, res) => {
-    if (!validate(req.headers)) res.end('unauthorized');
+app.get('/books', (req, res) => res.send('GET in /books'));
+app.get('/users',  (req, res) => res.send('GET in /users'));
+app.post('/users', (req, res) => res.send('POST in /users'));
 
-    if (req.url === '/books') {
-        switch (req.method) {
-            case 'GET': res.end('GET BOOKS'); break;
-            case 'POST': res.end('POST BOOKS'); break;
-            default: res.end('not supported')
-        }
-    } else if (req.url === '/users') {
-        switch (req.method) {
-            case 'GET': res.end('GET USERS'); break;
-            case 'POST': res.end('POST USERS'); break;
-            default: res.end('not supported')
-        }
-    }
-});
-
-const validate = headers => headers.authorization === 'authorized';
-
-httpServer.listen(port, () => {
+app.listen(3000, () => {
     console.log(`listening on ${port}`)
 });
